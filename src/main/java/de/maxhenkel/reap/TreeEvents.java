4a6a8a814049b353ac8fd99b1e30a7be4b17d176
CrossVas <cross.wanted.1.4@gmail.com>
2024-08-20 09:23:27 +0900

    better tree detection, avoid mining manually placed logs

--- src/main/java/de/maxhenkel/reap/TreeEvents.java
+++ src/main/java/de/maxhenkel/reap/TreeEvents.java
@@ -1,18 +1,22 @@
 package de.maxhenkel.reap;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
 
 public class TreeEvents {
 
@@ -24,9 +28,8 @@ public class TreeEvents {
         }
         Player player = event.getPlayer();
         BlockPos pos = event.getPos();
-        ItemStack heldItem = player.getMainHandItem();
-        if (canHarvest(pos, player, world, heldItem)) {
-            destroyTree(player, world, pos, heldItem);
+        if (canHarvest(player)) {
+            destroyTree(player, world, pos);
         }
     }
 
@@ -40,13 +43,14 @@ public class TreeEvents {
         if (pos == null) {
             return;
         }
-        if (canHarvest(pos, player, player.level(), player.getMainHandItem())) {
-            List<BlockPos> connectedLogs = getConnectedLogs(player.level(), pos);
+        if (canHarvest(player)) {
+            LinkedList<BlockPos> connectedLogs = scanForTree(player.level(), pos);
             event.setNewSpeed((float) (event.getOriginalSpeed() / Math.min(1D + Main.SERVER_CONFIG.dynamicTreeBreakingPerLog.get() * connectedLogs.size(), Main.SERVER_CONFIG.dynamicTreeBreakingMinSpeed.get())));
         }
     }
 
-    public static boolean canHarvest(BlockPos pos, Player player, Level world, ItemStack heldItem) {
+    public static boolean canHarvest(Player player) {
+        ItemStack heldItem = player.getMainHandItem();
         if (player.getAbilities().instabuild) {
             return false;
         }
@@ -59,77 +63,18 @@ public class TreeEvents {
             return false;
         }
 
-        if (!isLog(world, pos)) {
-            return false;
-        }
-
-        if (!isGround(world, pos.below())) {
-            return false;
-        }
-
-        BlockState state = world.getBlockState(pos);
-        if (state.getProperties().stream().anyMatch(p -> p.equals(RotatedPillarBlock.AXIS))) {
-            if (!state.getValue(RotatedPillarBlock.AXIS).equals(Direction.Axis.Y)) {
-                return false;
-            }
-        }
-
         return true;
     }
 
-    private static void destroyTree(Player player, Level world, BlockPos pos, ItemStack heldItem) {
-        List<BlockPos> connectedLogs = getConnectedLogs(world, pos);
-
-        for (BlockPos logPos : connectedLogs) {
-            destroy(world, player, logPos, heldItem);
+    private void destroyTree(Player player, Level world, BlockPos pos) {
+        LinkedList<BlockPos> connectedLogs = scanForTree(world, pos);
+        for (BlockPos log : connectedLogs) {
+            destroy(world, player, log);
         }
     }
 
-    private static List<BlockPos> getConnectedLogs(Level world, BlockPos pos) {
-        BlockPosList positions = new BlockPosList();
-        collectLogs(world, pos, positions);
-        return positions;
-    }
-
-    private static void collectLogs(Level world, BlockPos pos, BlockPosList positions) {
-        int maxHarvestingCount = Main.SERVER_CONFIG.treeHarvestMaxCount.get();
-        if (positions.size() >= maxHarvestingCount) {
-            return;
-        }
-        List<BlockPos> posList = new ArrayList<>();
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                for (int z = -1; z <= 1; z++) {
-                    BlockPos p = pos.offset(x, y, z);
-                    if (isLog(world, p)) {
-                        if (positions.size() <= maxHarvestingCount) {
-                            if (positions.add(p)) {
-                                posList.add(p);
-                            }
-                        } else {
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-
-        for (BlockPos p : posList) {
-            collectLogs(world, p, positions);
-        }
-    }
-
-    private static boolean isLog(Level world, BlockPos pos) {
-        BlockState b = world.getBlockState(pos);
-        return Main.SERVER_CONFIG.logTypes.stream().anyMatch(tag -> tag.contains(b.getBlock()));
-    }
-
-    private static boolean isGround(Level world, BlockPos pos) {
-        BlockState b = world.getBlockState(pos);
-        return Main.SERVER_CONFIG.groundTypes.stream().anyMatch(tag -> tag.contains(b.getBlock()));
-    }
-
-    private static void destroy(Level world, Player player, BlockPos pos, ItemStack heldItem) {
+    private static void destroy(Level world, Player player, BlockPos pos) {
+        ItemStack heldItem = player.getMainHandItem();
         if (heldItem != null) {
             heldItem.getItem().mineBlock(heldItem, world, world.getBlockState(pos), pos, player);
             world.destroyBlock(pos, true);
@@ -137,19 +82,85 @@ public class TreeEvents {
         }
     }
 
-    private static class BlockPosList extends ArrayList<BlockPos> {
-        @Override
-        public boolean add(BlockPos pos) {
-            if (!contains(pos)) {
-                return super.add(pos);
-            }
-            return false;
+    // tree detector
+    public static LinkedList<BlockPos> scanForTree(final LevelReader level, final BlockPos start) {
+        final BlockState blockState = level.getBlockState(start);
+        if (!blockState.is(BlockTags.LOGS)) {
+            return new LinkedList<>();
         }
 
-        @Override
-        public boolean contains(Object o) {
-            return stream().anyMatch(pos1 -> pos1.equals(o));
-        }
+        final boolean[] leavesFound = new boolean[1];
+        final LinkedList<BlockPos> result =
+                recursiveSearch(level, start, (pos, bs, isRightBlock) -> {
+                    if (isLeaves(bs)) {
+                        leavesFound[0] = true;
+                    }
+                    return true;
+                });
+        return leavesFound[0] ? result : new LinkedList<>();
     }
 
+    // for internal use
+    private interface BlockAction {
+        boolean onBlock(BlockPos pos, BlockState state, boolean isRightBlock);
+    }
+
+    // Recursively scan 3x3x3 cubes while keeping track of already scanned blocks to avoid cycles.
+    private static LinkedList<BlockPos> recursiveSearch(final LevelReader world, final BlockPos start, @Nullable final BlockAction action) {
+        final Block wantedBlock = world.getBlockState(start).getBlock();
+        boolean abort = false;
+        final LinkedList<BlockPos> result = new LinkedList<>();
+        final Set<BlockPos> visited = new HashSet<>();
+        final LinkedList<BlockPos> queue = new LinkedList<>();
+        int maxHarvestingCount = Main.SERVER_CONFIG.treeHarvestMaxCount.get();
+        queue.push(start);
+
+        while (!queue.isEmpty()) {
+            final BlockPos center = queue.pop();
+            final int x0 = center.getX();
+            final int y0 = center.getY();
+            final int z0 = center.getZ();
+            for (int z = z0 - 1; z <= z0 + 1 && !abort; ++z) {
+                for (int y = y0 - 1; y <= y0 + 1 && !abort; ++y) {
+                    for (int x = x0 - 1; x <= x0 + 1 && !abort; ++x) {
+                        final BlockPos pos = new BlockPos(x, y, z);
+                        final BlockState bs = world.getBlockState(pos);
+                        if ((bs.isAir() || !visited.add(pos))) {
+                            continue;
+                        }
+                        final boolean isRightBlock = bs.is(wantedBlock);
+                        if (isRightBlock) {
+                            result.add(pos);
+                            if (queue.size() > maxHarvestingCount) {
+                                abort = true;
+                                break;
+                            }
+                            queue.push(pos);
+                        }
+                        if (action != null) {
+                            abort = !action.onBlock(pos, bs, isRightBlock);
+                        }
+                    }
+                }
+            }
+        }
+        return !abort ? result : new LinkedList<>();
+    }
+
+    // leaves detector
+    // Naturally generated leaves don't have BlockStateProperties.PERSISTENT property, meaning it's a real tree.
+    // If there is BlockStateProperties.PERSISTENT property, it means the leave block is placed by player, meaning it's not a natural tree.
+    private static boolean isLeaves(final BlockState blockState) {
+        if (!(blockState.getBlock() instanceof LeavesBlock)) {
+            return false;
+        }
+        final Collection<Property<?>> properties = blockState.getProperties();
+        if (properties.contains(BlockStateProperties.PERSISTENT)) {
+            final boolean persistent = blockState.getValue(BlockStateProperties.PERSISTENT);
+            if (persistent) {
+                return false;
+            }
+        }
+        return blockState.getBlock() instanceof LeavesBlock;
+    }
 }
